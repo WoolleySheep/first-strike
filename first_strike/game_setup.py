e08d5aef6b9a1f6bd13e6636a280771ea69201a1
Matt Woolley <matt.wool@live.com.au>
2021-01-09 03:25:48 +1100

    Create a setup file for the game_data object

---
+++ first_strike/game_setup.py
@@ -0,0 +1,56 @@
+from game_data import (
+    Environment,
+    RocketProperties,
+    TurretProperties,
+    Properties,
+    RocketHistory,
+    TurretHistory,
+    History,
+    GameData,
+)
+
+WIDTH = 100.0
+HEIGHT = 100.0
+TIMESTEP = 0.1
+MAX_PLAYTIME = 1000
+
+MASS = 100.0
+ROCKET_TARGET_RADIUS = 5.0
+ROCKET_LENGTH = 3.0
+MAX_MAIN_ENGINE_FORCE = 100.0
+MAX_THRUSTER_FORCE = 50.0
+
+TURRET_TARGET_RADIUS = 5.0
+TURRET_LOCATION = (0.0, -50.0)
+MAX_ROTATION_SPEED = 1.0
+PROJECTILE_SPEED = 45.0
+MIN_FIRING_INTERVAL = 3.0
+
+ROCKET_START_LOCATION = (-50.0, 50.0)
+ROCKET_START_ANGLE = 0.0
+
+TURRET_START_ANGLE = 0.0
+
+environment = Environment(WIDTH, HEIGHT, TIMESTEP, MAX_PLAYTIME)
+
+rocket_properties = RocketProperties(
+    MASS,
+    ROCKET_TARGET_RADIUS,
+    ROCKET_LENGTH,
+    MAX_MAIN_ENGINE_FORCE,
+    MAX_THRUSTER_FORCE,
+)
+turret_properties = TurretProperties(
+    TURRET_TARGET_RADIUS,
+    TURRET_LOCATION,
+    MAX_ROTATION_SPEED,
+    PROJECTILE_SPEED,
+    MIN_FIRING_INTERVAL,
+)
+properties = Properties(rocket_properties, turret_properties)
+
+rocket_history = RocketHistory([ROCKET_START_LOCATION], [ROCKET_START_ANGLE])
+turret_history = TurretHistory([TURRET_START_ANGLE])
+history = History(rocket_history, turret_history)
+
+game_data = GameData(environment, properties, history)
